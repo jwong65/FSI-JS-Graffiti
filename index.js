14ab10f13ef8f89bfb5d911de9cfdda33f2a8f33
unknown <jason.wong6507@gmail.com>
2022-08-29 20:06:24 -0400

    Completed activity in the index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let header = document.querySelector("#page-header")
+header.style.textAlign="right"
+let dogimages = document.querySelectorAll(".dog-image")
+for (let i =0; i< dogimages.length; i++)
+{
+    dogimages[i].style.borderRadius = '25px'
+}
+let dogname = document.querySelectorAll(".dog-name")
+for (let i =0; i<dogname.length; i++)
+{
+    dogname[i].style.textAlign="left"
+}
+let footertext =document.querySelector(".footer")
+footertext.style.color="red"
 
+//for (let u=0; u<dogimages.length; u++)
+//{}
 // Select some elements...
